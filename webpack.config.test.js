58a55ed61a343257de794601eaa330f6fd579b34
haoyu bao <haoyu bao>
2017-05-15 16:59:08 +0800

    d

---
+++ webpack.config.test.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const webpack = require('webpack');
+module.exports = {
+    entry: './test/index.js',
+
+    output: {
+        // 输出文件名
+        filename: 'bundle.js',
+
+        // 输出目录
+        path: path.resolve(__dirname, '__build__'),
+    },
+
+    watch: !0,
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    'babel-loader',
+                ],
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.styl$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: !0
+                        }
+                    },
+                    'stylus-loader',
+                ]
+            },
+        ],
+    },
+    plugins: [
+        // mini
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
+        // env
+        // new webpack.DefinePlugin({
+        //     'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+        // }),
+    ]
+};
